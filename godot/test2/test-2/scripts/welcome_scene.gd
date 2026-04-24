614b191023adf51f63278314da4d88b278469a5f
NGDenpa <xueyang0327@gmail.com>
2026-04-25 02:57:38 +0800

    标题画面、credits

--- godot/test2/test-2/scripts/welcome_scene.gd
+++ godot/test2/test-2/scripts/welcome_scene.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
+
+func like():
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
